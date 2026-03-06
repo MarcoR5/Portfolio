aa5fb157e80ed6682fa423644698978f54689ae3
MarcoR5 <marcowork50@gmail.com>
2026-03-06 11:09:02 +0200

    TEST3

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: { unoptimized: true },
-  basePath: "/portfolio",
+  basePath: "/Portfolio",
 };
 
 export default nextConfig;
